cbfae0a7108e09e6fb45514ddee5287dae20e6ef
Chris Benard <chris@chrisbenard.net>
2012-04-03 10:13:22 -0500

    Separate project

---
+++ SeparateProjectExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ServiceProject.WeatherService;
+
+namespace SeparateProjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new WeatherSoapClient();
+            var weatherReturn = service.GetCityWeatherByZIP("75074");
+
+            Console.WriteLine("City length from partial: " + weatherReturn.CityLength);
+        }
+    }
+}
---
+++ ServiceProject/WeatherReturnPartial.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceProject.WeatherService
+{
+    public partial class WeatherReturn
+    {
+        public int CityLength
+        {
+            get { return City.Length; }
+        }
+    }
+}
